803b9d0cfff574c376350d7651720da36d262359
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-05-25 12:24:12 +0930

    End of Day 4 Intro to C
    Various changes to project too, most of which I am not sure what they do: but did turn off address randomization for debug

--- vs2013/test/test/test.cpp
+++ vs2013/test/test/test.cpp
@@ -1,14 +1,16 @@
 #include <windows.h>
 #include <stdio.h>
 
-// Function definition
-void foo()
+//#pragma pack(1) : force boundary size (tight/loose packing)
+struct projectile
 {
-	char* Foo = "This is the first thing we have actually printed.\n";
-	OutputDebugStringA(
-		"This is the first thing we have actually printed.\n");
-	printf("This is a thing.\n");
-}
+	char unsigned IsThisOnFire;
+	int Damage;
+	int ParticlesPerSecond;
+	short HowManyCooks;
+
+	// tightly compact => 11bytes. Optimized for CPU will be 16 bytes
+};
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -17,9 +19,60 @@ int CALLBACK WinMain(
 	int nCmdShow
 	)
 {
-	char unsigned Test;
-	Test = 255;
-	Test = Test + 1;
+	projectile Test;
 
-	foo();
+	int SizeOfProjectile = sizeof(projectile);
+	
+	Test.IsThisOnFire = 1;
+	Test.Damage = 1 + Test.IsThisOnFire;
+	Test.ParticlesPerSecond = 3;
+	Test.HowManyCooks = 4;
+
+	projectile *ProjectilePointer = &Test;
+	short unsigned  *MrPointerMan = (short unsigned *)&Test;
+
+	projectile Projectiles[40];
+	projectile *Projectiles_ARR_Pointer = Projectiles;
+
+	int SizeOfProjectiles = sizeof(Projectiles); //16*40
+	int SizeOfProjectiles_ARR_Pointer = sizeof(Projectiles_ARR_Pointer); //4
+
+	Projectiles[30].Damage = 60;
+
+	//-- Adding this the ultra clarity: will be uninitialised and appear as -52(signed) or 204(unsigned), but clearer is even better
+	Projectiles[30].IsThisOnFire = 1;
+	
+	// For when we just point to the first byte
+	Projectiles[0].IsThisOnFire = 2;
+	//---
+
+	// Same as:
+	(Projectiles_ARR_Pointer + 30)->Damage = 100;
+	// Or:
+	(*(Projectiles_ARR_Pointer + 30)).Damage = 110;
+
+	// Note you do NOT do this: won't be what you expect: C understands +30 as 30*sizeof(...) already;
+	// It will still 'work' (if the array is big enough - otherwise access violation) but will be much
+	// further along!
+	// Lets test this:
+	// This will adjust the 30th value (2*16 -2 = 30 :
+	(Projectiles_ARR_Pointer + 2 * sizeof(projectile) -2)->Damage = 111222333;
+
+	// To use the sizeof you need to cast Projectile_Arr_Pointer to a char * (point to char; 1 byte) then add 30*sizeof(projectile)
+	((projectile *)((char *)Projectiles_ARR_Pointer + 30 * sizeof(projectile)))->Damage = 120;
+
+	//-- Broken down:
+	char *BytePointer = (char *)Projectiles_ARR_Pointer;
+	BytePointer = BytePointer + 30 * sizeof(projectile);
+
+	//BUT NOW:Can't do:
+	// BytePointer->Damage = 100 : BytePointer is _not_ a pointer to Projectile_ARR_Pointer, only a pointer to 8 bits (however
+	// it does point to the first 8 bits (1st byte) of Projectiles (the array).
+
+	// So need to do:
+	projectile *Thirty = (projectile *)BytePointer;
+	Thirty->Damage = 130;
+	//--
+
+
 }
