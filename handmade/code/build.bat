10b898e6f4bc40d28db36984b492205e1b305714
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-06-08 20:04:51 +0930

    build.bat can be run from anywhere; extra paths added to curr cmd path

--- handmade/code/build.bat
+++ handmade/code/build.bat
@@ -1,8 +1,18 @@
 @echo off
 
+:: if not running from the code dir, change it to there! 
+set handmade_code_dir=w:\handmade\code
+if not %CD%==%handmade_code_dir% pushd %handmade_code_dir% 
+
+:: build in the build dir
 mkdir ..\build
 pushd ..\build
 cl -Zi w:\handmade\code\win32_handmade.cpp user32.lib Gdi32.lib
 rem Zi enables debug info. user32.lib eg for MessageBox
+
+:: get out of build dir
+popd
+
+:: get back to where we started
 popd
 
--- handmade/misc/shell.bat
+++ handmade/misc/shell.bat
@@ -1,3 +1,3 @@
 @echo off
 call "E:\VisualStudio\2013\VC\vcvarsall.bat" x64
-set path=w:\handmade\misc;%path%
+set path=w:\handmade\misc;w:\handmade\build;w:\handmade\code;%path%
