21aa77f74b20452e7dd909d29885f8bf8b8da237
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-06-29 19:49:37 +0930

    Q&A Day 002 completed

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -8,6 +8,16 @@
 
     #include <windows.h>
 
+    // Undefine the following to forcce use of ANSI or UNICODE versions - we will always use ANSI,
+    // so doing the below will: 1) make us use suffix A 2) If someone tries to compile in Unicode mode, an error will occur (good)
+    #undef CreateWindowEx
+    #undef OutputDebugString
+    #undef TranslateMessage
+    #undef DispatchMessage
+    #undef DefWindowProc 
+
+
+
     LRESULT CALLBACK
     MainWindowCallback(HWND WindowHandle,
                        UINT Message,
@@ -16,10 +26,12 @@
     {
         LRESULT Result = 0; // Zero for now
 
-        // Note Casey's use of {} in case - so that he can keep an variable declared local to a case block, rather than the whole switch block; break outside no other
+        // Note Casey's use of {} in case - so that he can keep an variable declared local to a case block,
+        // rather than the whole switch block; break outside no other
         // reason than simpler to read, and maybe not removed by mistake, getting mixed up with other code
         switch(Message)
         {
+
             case WM_SIZE:
             {
                 OutputDebugStringA("WM_SIZE\n");
@@ -99,12 +111,13 @@
                 }
 
 
+                SetPixel(DeviceContext, 100, 100, RGB(255,0,0));
                 EndPaint(WindowHandle, &Paint);
             } break;
 
             default:
             {
-                Result = DefWindowProc(WindowHandle, Message, WParam, LParam);
+                Result = DefWindowProcA(WindowHandle, Message, WParam, LParam);
             } break;
         }
 
@@ -119,16 +132,30 @@
            LPSTR CmdLine,
            int ShowCmd)
     {
+        int * IntPtr = 0;
+        
         WNDCLASS WindowClass = {}; //set all to zero - C++; C may need {0}
 
-        WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW; //MSDN for more detail
+        // Don't need WindowsClass.style _at all_  apparently Casey (or I can if I want) check
+        WindowClass.style = CS_OWNDC;//|CS_HREDRAW|CS_VREDRAW; //MSDN for more detail
+
+        // Casey: We need to know the callback name here, because as we saw around 30min at Day 2 Q&A, windows can ask for it
+        // at anytime (before _we_ dispatch it). Like Casey says, would be better if we didn't need to define the WindowProc callback
+        // ourselves.
+        // 
+        // Remove it and see - you will get a runtime error, as windows wants to call it.
+        //
+        //BUT check this out! I decided to set it to DefWindowProc; And we are back in business! BUT ofcourse we are then not 
+        //able to handle messages. We could still enter the default case in our implementation of the callback and print out the message
+        //and maybe even map it to the name using a table we can make for eg. the WINE website with windows messages and their values.
         WindowClass.lpfnWndProc = MainWindowCallback;
+        //WindowClass.lpfnWndProc = DefWindowProc;
         WindowClass.hInstance = Instance; // could call kernel function GetModuleHandle(0)
         WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
-        if(RegisterClass(&WindowClass))
+        if(RegisterClassA(&WindowClass))
         {
-            HWND WindowHandle =  CreateWindowEx(
+            HWND WindowHandle =  CreateWindowExA(
                                   0,
                                   WindowClass.lpszClassName,
                                   "HandMadeHero",
@@ -146,11 +173,11 @@
                 MSG Message;
                 for(;;)
                 {
-                    BOOL MessageResult = GetMessage(&Message,0, 0, 0);
+                    BOOL MessageResult = GetMessageA(&Message,0, 0, 0);
                     if(MessageResult > 0)
                     {
                         TranslateMessage(&Message);
-                        DispatchMessage(&Message);
+                        DispatchMessageA(&Message);
                     }
                     else
                     {
