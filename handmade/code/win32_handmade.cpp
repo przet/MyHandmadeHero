43c970dfb8d7b62fbc53ffcc793716f2a9db3590
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-09-07 14:46:10 +0930

    Day 004 completed! Work with 32-bit pixel directly

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -52,16 +52,34 @@
 
         for (int Y = 0; Y < BitmapHeight; ++Y)
         {
-            uint8 *Pixel = Row;
+            uint32 *Pixel = (uint32 *)Row;
             for (int X = 0; X < BitmapWidth; ++X)
             {
-                *Pixel++ = X + XOffset;
+                // Explanation:
+                // 
+                // Memory(hex) : BB GG RR xx (xx is pad)
+                // Register (hex. Assuming little endian architecture) xx RR GG BB
+                //
+                // So now that we are dealing with 32 bits directly (i.e 32-bit pixel) we rewrite
 
-                *Pixel++ = Y + YOffset;
+                /*
+                    *Pixel++ = X + XOffset;
 
-                *Pixel++ = 0;
+                    *Pixel++ = Y + YOffset;
 
-                *Pixel++ = 0; 
+                    *Pixel++ = 0;
+
+                    *Pixel++ = 0;
+                */
+
+                // as
+                uint8 Blue = X + XOffset;
+                uint8 Green = Y + YOffset;
+                uint8 Red = 0;
+                uint8 Pad = 0;
+
+                *Pixel++ = (Blue | Green << 8 | Red << 16 | Pad << 24);
+                // TODO : operator precedence : remove brackets
             }
 
             Row += Pitch;
