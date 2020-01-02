1057604d2c3432d2e58024d8f8592988198d9fde
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2020-01-03 08:47:34 +1030

    Day007: XinputStubs return ERROR_DEVICE_NOT_CONNECTED
    0 in windows world is ERROR_SUCCESS... (usually)

--- handmade/code/XInputStubs.h
+++ handmade/code/XInputStubs.h
@@ -1,9 +1,9 @@
 X_INPUT_GET_STATE(XInputGetStateStub)
 {
-    return 0;
+    return ERROR_DEVICE_NOT_CONNECTED;
 }
 
 X_INPUT_SET_STATE(XInputSetStateStub)
 {
-    return 0;
+    return ERROR_DEVICE_NOT_CONNECTED;
 }
